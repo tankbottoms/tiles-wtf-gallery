41c3f2426290163b5a636d54f3acf7e5bb030383
Evie <evita@stenqvist.co>
2022-06-01 12:01:29 +0200

    Placeholder FAQ

---
+++ src/routes/faq.svelte
@@ -0,0 +1,13 @@
+<section>
+    <h1>FAQ</h1>
+    <p>Coming soon.</p>
+</section>
+
+<style>
+	section {
+		max-width: 540px;
+		padding: 20px 20px 100px;
+		line-height: 1.3;
+		margin: 10vh auto;
+	}
+</style>
