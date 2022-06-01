486b2aa8d2f9e8bfc349fded7e22c4dc41831554
Evie <evita@stenqvist.co>
2022-06-01 11:59:16 +0200

    About page

---
+++ src/routes/about.svelte
@@ -0,0 +1,42 @@
+<section>
+	<h1>About</h1>
+
+	<p>
+		Tiles v2 is an on-chain re-implementation of <a href="https://twitter.com/peripheralist"
+			>@peripheralist</a
+		>’s <a href="https://github.com/TileDAO">tilesdao</a> the
+		<a href="https://juicebox.money/#/p/tiles">second project on Juicebox</a>. The project is an
+		homage to the venerable @peripheralist, the genius artist, developer, person.
+	</p>
+	<p>
+		<a href="https://github.com/tankbottoms/tiles-on-chain">Tiles v2 on chain</a> contains the
+		re-implementation. The contract structure employed follows a composable pattern which will be
+		familiar with anyone involved in the Juicebox NFT minting toolkit. The Tile contract uses
+		Juicebox v2 ProjectPayer (<a
+			href="https://info.juicebox.money/docs/protocol/build/utilities/project-payer"
+			>https://info.juicebox.money/docs/protocol/build/utilities/project-payer</a
+		>) as its transparent treasury.
+	</p>
+	<p>
+		Original Tiles v1 token holders may mint their token for free (sans gas), if someone has
+		preemptively minted another individual&#39;s address, a small premium would be required to seize
+		the token.
+	</p>
+</section>
+
+<style>
+	section {
+		max-width: 540px;
+		padding: 20px 20px 100px;
+		line-height: 1.3;
+		margin: 10vh auto;
+	}
+
+	a {
+		white-space: pre-wrap; /* CSS3 */
+		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+		white-space: -pre-wrap; /* Opera 4-6 */
+		white-space: -o-pre-wrap; /* Opera 7 */
+		word-wrap: break-word; /* Internet Explorer 5.5+ */
+	}
+</style>
