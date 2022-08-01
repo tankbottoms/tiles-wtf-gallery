a01efd4e081f4b4bff606a5c382890e71f106015
tankbottoms <m@tankbottoms.xyz>
2022-08-01 10:20:53 -0400

    Updated faq

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,12 +1,11 @@
 <section>
     <h1>FAQ</h1>
-    <b>WTF?</b>
-	<p>TBD</p>
-</section>
-<section>
-    <h1>FAQ</h1>
-    <b>WTF?</b>
-	<p>TBD</p>
+    <b>Wtf?</b>
+	<p>See <a href="/wtf" target="_blank">Wtf</a></p>	
+    <b>Purpose?</b>
+	<p>If you are blind and have no appreciation for art, algorithmic or otherwise, then the purpose of this site is to demonstrate a project application, like nft minting which routes the proceeds to a Juicebox treasury.</p>	
+	<b>Use of proceeds?</b>
+	<p>The funds earned by purchases of the Tiles will be allocated in the following way.</p>
 </section>
 
 <style>
