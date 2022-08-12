e879a3af7eb388dd8d0d91102dfa60c6821f9439
tankbottoms <m@tankbottoms.xyz>
2022-08-12 19:25:04 -0400

    added ramblings to the wtf section

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -4,10 +4,10 @@
 	<p>see <a href="/wtf" target="_blank">wtf</a></p>	
     <b>purpose of this project?</b>
 	<p>reset and slow the pricing function of the original tiles to be accessible. implement tiles to be completely on chain. distribute proceeds to the juicebox community. open source a fully operational svelte <a href="https://juicebox.money/v2/p/41" target="_blank">juicebox treasury</a>.</p>	
-	<b>how will the tiles v1.2 proceeds be used?</b>
+	<b>how will the tiles v2 proceeds be used?</b>
 	<p>tbd</p>
 	<b>do existing Tiles owners pay anything?</b>
-	<p>no.  existing <a href="https://etherscan.io/address/0x64931F06d3266049Bf0195346973762E6996D764" target="_blank">infinite tile</a> owners may mint for free, this is called <a href="https://github.com/tankbottoms/tiles-on-chain/blob/main/contracts/TileNFT.sol#L124" target="_blank">grabbing</a>.</p>
+	<p>no.  owners of the original <a href="https://etherscan.io/address/0x64931F06d3266049Bf0195346973762E6996D764" target="_blank">infinite tile</a> mint for free, this is called <a href="https://github.com/tankbottoms/tiles-on-chain/blob/main/contracts/TileNFT.sol#L124" target="_blank">grabbing</a>.</p>
 	<b>how are the tiles priced?</b>
 	<p>the base price will start out as <b>0.0001 ETH and be multipled by 2 every 512 tiles minted</b>, thus the next teir after the first will be priced as 0.0002 for 512 tiles and so on.</p>
 	<p>details maybe found in the <a href="https://github.com/tankbottoms/tiles-on-chain/blob/main/scripts/mainnet.ts" target="_blank">deploy script</a>, the Tiles has what is called the <a href="https://github.com/tankbottoms/tiles-on-chain/blob/main/contracts/LegacyOwnershipPriceResolver.sol">LegacyOwnershipPriceResolver</a> which governs how pricing is resolved.  
