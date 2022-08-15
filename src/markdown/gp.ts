94ff7c968767b5b25f8857afe53bbd7cdad146e7
tankbottoms <m@tankbottoms.xyz>
2022-08-15 18:19:09 -0400

    Created definitions so the project would resolve

--- src/markdown/gp.ts
+++ src/markdown/gp.ts
@@ -4,7 +4,7 @@ export default `
 
 ## Definitions.
 
--   The [definitions](definitions) section of the associated document is included in its entirety herein. Any terms which are contained herein supersede any definition contained in the associated document.
+-   The [definitions](./definitions) section of the associated document is included in its entirety herein. Any terms which are contained herein supersede any definition contained in the associated document.
 
 -   **"Service Provider"** means the Person or Persons appointed by the DAO to perform administrative services, responsibilities, and duties to carry on the DAO's operations. The initial Service Provider shall be [dao-lawfirm.eth](https://etherscan.io/address/0x752515a3a1091b9f1c04416cf79d1f14d2340085), its predecessor entities, or any future entity of [dao-lawfirm.eth](https://etherscan.io/address/0x752515a3a1091b9f1c04416cf79d1f14d2340085).
 
@@ -12,9 +12,9 @@ export default `
 
 ## Introduction.
 
-These guiding principles constitute an Agreement (hereinafter the "Agreement") and are entered into by and amongst members of EXAMPLE DAO (hereinafter “DAO”), an unincorporated nonprofit association organized under the laws of the State of Delaware.
+These guiding principles constitute an Agreement (hereinafter the "Agreement") and are entered into by and amongst members of Infinite Tiles v2 DAO (hereinafter “DAO”), an unincorporated nonprofit association organized under the laws of the State of Delaware.
 
-The following terms apply when you click to view or access DAO's Dapp, DAO's online services (including https://example.xyz), or any other of DAO's sites, donate or transfer cryptocurrencies such as Ethereum, or transfer one or more ERC-20 ("Tokens") or ERC-721 ("NFTs") or other digital assets (collectively, "Cryptographic currencies") to the DAO, become a Member of DAO, receive DAO Governance Tokens, hold Governance Rights for the DAO, interact with or access [the DAO's smart contracts](/example-addresses) in any way, provide services to the DAO, donate or transfer any property to the DAO, or otherwise interact with or access any other of the DAO's services. The DAO's governance may occur on [Snapshot](https://snapshot.org), however for the purpose of this agreement we refer to this as part of the DAO Dapp.
+The following terms apply when you click to view or access DAO's Dapp, DAO's online services (including https://tiles.wtf), or any other of DAO's sites, donate or transfer cryptocurrencies such as Ethereum, or transfer one or more ERC-20 ("Tokens") or ERC-721 ("NFTs") or other digital assets (collectively, "Cryptographic currencies") to the DAO, become a Member of DAO, receive DAO Governance Tokens, hold Governance Rights for the DAO, interact with or access [the DAO's smart contracts](/example-addresses) in any way, provide services to the DAO, donate or transfer any property to the DAO, or otherwise interact with or access any other of the DAO's services. The DAO's governance may occur on [Snapshot](https://snapshot.org), however for the purpose of this agreement we refer to this as part of the DAO Dapp.
 
 **By doing any of the above, you signify your agreement to these terms. If you do not agree to be bound by the Agreement in its entirety, you may not access, interact with, or use the DAO Dapp or online services.**
 
