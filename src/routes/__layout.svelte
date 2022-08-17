7ff6790edf82d006212958f58a8c70b3de7b8b90
Evie <evita@stenqvist.co>
2022-08-17 13:12:07 +0200

    Terms of sale on every page in footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,7 @@
 	import GrabHistoryProvider from '$components/GrabHistoryProvider.svelte';
 	import Loading from '$juicebox/components/Loading.svelte';
 	import { pageReady } from '$stores';
+	import { readNetwork } from '$stores/web3';
 
 	onMount(logInfiteTilesAscii);
 	onMount(async () => {
@@ -27,10 +28,27 @@
 			<slot />
 			<ErrorModal />
 		{/if}
+		<footer>
+			<div class="footer">
+				<div class="link">
+					<a href="/tos?{$readNetwork ? `network=${$readNetwork?.alias}` : ''}">Terms of sale</a>
+				</div>
+			</div>
+		</footer>
 	</GrabHistoryProvider>
 </Intl>
 
 <style>
+	.footer {
+		margin: auto;
+		font-size: 14px;
+		color: black;
+		text-decoration: none;
+		align-items: center;
+		justify-items: center;
+		text-align: center;
+		margin: 0px 5px;
+	}
 	:global(body) {
 		cursor: crosshair;
 		max-width: 100vw;
