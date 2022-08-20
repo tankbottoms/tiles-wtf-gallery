ae25e73b48710aa4734dda7a973f4989ad57e118
EVMCompatible <evmcompatible@gmail.com>
2022-08-21 01:03:18 +0500

    add ipfs-404.html s to routes not working on fleek

---
+++ src/routes/render/png/ipfs-404@root.svelte
@@ -0,0 +1,9 @@
+<!-- DO NOT DELETE THIS FILE -->
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto(window.location.href.replace(window.location.origin, ''), { replaceState: true });
+	});
+</script>
