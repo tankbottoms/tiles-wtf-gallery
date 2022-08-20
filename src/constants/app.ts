72a0165028cee36d3d93c3fc43aa47790fcfa378
EVMCompatible <evmcompatible@gmail.com>
2022-08-21 01:02:50 +0500

    update mainnet tiles address

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-	contract_mainnet: '0xa0dadfA9279C1379CfcAbd8e21Af78b9064cCeb0',
+	contract_mainnet: '0x4ddeF8Fc8EEE89848b4A802CEF9FC9E72B8674A4',
 	contract_rinkeby: '0x3A31414dFbA8B20D3bDa767092984db9d98a2da1',
 	project_mainnet: 175,
 	project_rinkeby: 98
